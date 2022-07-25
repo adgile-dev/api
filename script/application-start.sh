94beac6490a0bc9fd8da2c576f152d57aa1eec93
doyeon kim <doyeonkim@doyeonui-MacBookPro.local>
2022-07-25 11:01:53 +0900

    fix(appspec): deploy_path modify

--- script/application-start.sh
+++ script/application-start.sh
@@ -5,7 +5,7 @@ JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/
+DEPLOY_PATH=/home/ubuntu/adgile/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인 " >> /home/ubuntu/deploy.log
@@ -21,5 +21,7 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+
+echo "> DEPLOY_PATH: $DEPLOY_PATH"
 echo "> DEPLOY_JAR 배포 " >> /home/ubuntu/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_error.log &
